28f1bc6f1cae42c13035c36118bb7d7fddd56f67
Aurino Geraldo <aurinoaj@gmail.com>
2020-10-26 10:16:46 -0300

    Start repositories list, html and stylesheet

--- src/pages/Dashboard/DashboardStyles.ts
+++ src/pages/Dashboard/DashboardStyles.ts
@@ -17,6 +17,11 @@ export const Form = styled.form`
     border: 1px solid #a3a3a3;
     border-radius: 5px 0 0 5px;
     padding: 0 24px;
+    color: #3a3a3a;
+
+    &::placeholder {
+      color: #a8a8b3;
+    }
   }
 
   button {
@@ -28,5 +33,54 @@ export const Form = styled.form`
     font-weight: bold;
     height: 70px;
     width: 210px;
+  }
+`
+
+export const Repositories = styled.div`
+  margin-top: 80px;
+
+  a {
+    align-items: center;
+    background: #fff;
+    border-radius: 5px;
+    display: flex;
+    padding: 24px;
+    text-decoration: none;
+    transition: 0.2s;
+
+    & + a {
+      margin-top: 10px;
+    }
+
+    &:hover {
+      transform: translateX(10px);
+    }
+
+    img {
+      border-radius: 50%;
+      height: 64px;
+      width: 64px;
+    }
+
+    div {
+      margin-left: 16px;
+
+      strong {
+        color: #3a3a3a;
+        font-size: 20px;
+      }
+
+      p {
+        color: #a8a8b3;
+        font-size: 18px;
+        margin-top: 4px;
+      }
+
+    }
+    .icon-rep {
+      color: #a8a8b3;
+      font-family: 'Nanum Gothic', sans-serif;
+      margin-left: auto;
+    }
   }
 `
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 import logo from '../../assets/logo-github.svg'
 
-import { Title, Form } from './DashboardStyles'
+import { Title, Form, Repositories } from './DashboardStyles'
 
 const Dashboard: React.FC = () => {
   return (
@@ -17,6 +17,35 @@ const Dashboard: React.FC = () => {
         />
         <button type="submit">Pesquisar</button>
       </Form>
+
+      <Repositories>
+        <a href="#">
+          <img src="https://avatars2.githubusercontent.com/u/32946164?s=400&u=94f8c4fca707024de0c6e94a271219509e39c2f2&v=4" alt="Foto de usuário"/>
+          <div>
+            <strong>Github-explore</strong>
+            <p>Aplicação para explorar repositório de usuários no github.</p>
+          </div>
+          <span className="icon-rep">{'>'}</span>
+        </a>
+
+        <a href="#">
+          <img src="https://avatars2.githubusercontent.com/u/32946164?s=400&u=94f8c4fca707024de0c6e94a271219509e39c2f2&v=4" alt="Foto de usuário"/>
+          <div>
+            <strong>Github-explore</strong>
+            <p>Aplicação para explorar repositório de usuários no github.</p>
+          </div>
+          <span className="icon-rep">{'>'}</span>
+        </a>
+
+        <a href="#">
+          <img src="https://avatars2.githubusercontent.com/u/32946164?s=400&u=94f8c4fca707024de0c6e94a271219509e39c2f2&v=4" alt="Foto de usuário"/>
+          <div>
+            <strong>Github-explore</strong>
+            <p>Aplicação para explorar repositório de usuários no github.</p>
+          </div>
+          <span className="icon-rep">{'>'}</span>
+        </a>
+      </Repositories>
     </>
   )
 }
